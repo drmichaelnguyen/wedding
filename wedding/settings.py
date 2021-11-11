142ac4009c3a7134a158e0a4933b5335d74439fd
drmichael <dr.trongto@gmail.com>
2021-11-11 16:36:50 +0700

    try static

--- wedding/settings.py
+++ wedding/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+static_dir=Path(BASE_DIR)/'static'
 
 
 # Quick-start development settings - unsuitable for production
@@ -117,8 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static_files/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+STATICFILES_DIRS=[
+    static_dir,
+    os.path.join(BASE_DIR, 'static'),
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "static_media")
